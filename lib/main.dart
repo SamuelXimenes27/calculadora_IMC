30423c9fbc49f3bf515f2fc78299873a7b2330b1
Samuel Ximenes <71338886+SamuelXimenes27@users.noreply.github.com>
2022-01-20 09:16:39 -0300

    Refatoração de uma parte do código.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(Home());
 
 class Home extends StatefulWidget {
   @override
@@ -26,7 +22,6 @@ class _HomeState extends State<Home> {
       _infoText = "Informe seus dados!";
       _formKey = GlobalKey<FormState>();
     });
-
   }
 
   void _calculate() {
@@ -55,67 +50,67 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calculadora de IMC"),
+          title: const Text("Calculadora de IMC"),
           centerTitle: true,
           backgroundColor: Colors.green,
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.refresh),
+              icon: const Icon(Icons.refresh),
               onPressed: _resetFields,
             )
           ],
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
           child: Form(
             key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Icon(
+                const Icon(
                   Icons.person_outline,
                   size: 120.0,
                   color: Colors.green,
                 ),
                 TextFormField(
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                       labelText: "Peso (kg)",
                       labelStyle: TextStyle(color: Colors.green)),
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  style: const TextStyle(color: Colors.green, fontSize: 25.0),
                   controller: weightController,
                   validator: (value) {
-                    if(value.isEmpty){
+                    if (value == null || value.isEmpty) {
                       return "Insira seu Peso";
                     }
                   },
                 ),
                 TextFormField(
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                       labelText: "Altura (cm)",
                       labelStyle: TextStyle(color: Colors.green)),
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  style: const TextStyle(color: Colors.green, fontSize: 25.0),
                   controller: heightController,
                   validator: (value) {
-                    if(value.isEmpty){
+                    if (value == null || value.isEmpty) {
                       return "Insira sua Altura";
                     }
                   },
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Container(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: SizedBox(
                     height: 50.0,
                     child: RaisedButton(
-                      onPressed: (){
-                        if(_formKey.currentState.validate()){}
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {}
                         _calculate();
                       },
-                      child: Text(
+                      child: const Text(
                         "Calcular",
                         style: TextStyle(color: Colors.white, fontSize: 25.0),
                       ),
@@ -126,7 +121,7 @@ class _HomeState extends State<Home> {
                 Text(
                   _infoText,
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  style: const TextStyle(color: Colors.green, fontSize: 25.0),
                 )
               ],
             ),
